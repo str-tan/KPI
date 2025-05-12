05bb96e312400460a124f4ed00c4dd6461e225d7
[КП-43] Стрілецька Тетяна <tanyast06@gmail.com>
2025-05-12 21:30:03 +0300

    Task7.

--- KPIPractice2/KPIPractice2/Generics II/Task7.cs
+++ KPIPractice2/KPIPractice2/Generics II/Task7.cs
@@ -6,7 +6,57 @@
     // - Метод void SendPackage<T>(T c) where T:DeliveryCenter,IDelivery { c.Deliver(); }.
     public class Task7
     {
-        public void Main() { }
+        public void Main()
+        {
+            var f = new Factory<DeliveryCenter>();
+            DeliveryCenter p = f.Create();
+            p.Location = "Lviv";
+            p.PrintInfo();
+
+            NovaPoshta np = new NovaPoshta();
+            np.Location = "Kyiv";
+           // SendPackage(np);
+        }
+
+        public static void SendPackage<T>(T c) where T : DeliveryCenter, IDelivery
+        {
+            c.Deliver();
+        }
+
+    }
+}
+
+
+
+
+    public class Factory<T> where T : class, new()
+    {
+        public T Create()
+        {
+            return new T();
+        }
     }
 
-}
+    public class DeliveryCenter
+    {
+        public string Location { get; set; } = "Unknown";
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"DeliveryCenter at: {Location}");
+        }
+    }
+
+    public interface IDelivery
+    {
+        void Deliver();
+    }
+
+    public class NovaPoshta : DeliveryCenter, IDelivery
+    {
+        public void Deliver()
+        {
+            Console.WriteLine($"NovaPoshta delivers from {Location}");
+        }
+    }
+
