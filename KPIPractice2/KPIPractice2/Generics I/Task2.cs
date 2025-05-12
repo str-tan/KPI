bb3e3aee749b56d08f322b4c09e90862365728d5
[КП-43] Стрілецька Тетяна <tanyast06@gmail.com>
2025-05-12 21:28:07 +0300

    Task2.

--- KPIPractice2/KPIPractice2/Generics I/Task2.cs
+++ KPIPractice2/KPIPractice2/Generics I/Task2.cs
@@ -1,11 +1,29 @@
 ﻿namespace KPIPractice2
-{ 
+{
     //  Swap<T> — обмін значень
     // - Напишіть статичний метод void Swap<T>(ref T a, ref T b).
     // - У Main обміняйте int x=5,y=10 і string s1="Hello",s2="World".
     public class Task2
     {
-        public void Main() { }
+        public void Main()
+        {
+            int x = 7, y = 34;
+            Console.WriteLine($"До обміну: x = {x}, y = {y}");
+            Swap(ref x, ref y);
+            Console.WriteLine($"Після обміну: x = {x}, y = {y}");
+
+            string s1 = "Hello", s2 = "World";
+            Console.WriteLine($"До обміну: s1 = {s1}, s2 = {s2}");
+            Swap(ref s1, ref s2);
+            Console.WriteLine($"Після обміну: s1 = {s1}, s2 = {s2}");
+        }
+
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
     }
 
 }
