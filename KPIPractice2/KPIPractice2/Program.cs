705c1d114d41c1a31159e7b9dc47c357c6eec972
[КП-43] Стрілецька Тетяна <tanyast06@gmail.com>
2025-05-12 21:27:15 +0300

    Execute tasks.

--- KPIPractice2/KPIPractice2/Program.cs
+++ KPIPractice2/KPIPractice2/Program.cs
@@ -2,9 +2,41 @@
 
 ExecuteTasks();
 
-
+//запускати тут
 void ExecuteTasks()
 {
     Task1 task1 = new Task1();
     task1.Main();
+
+    Console.WriteLine();
+
+    Task2 task2 = new Task2();
+    task2.Main();
+
+    Console.WriteLine();
+
+    Task3 task3 = new Task3();
+    task3.Main();
+
+    Console.WriteLine();
+
+    Task4 task4 = new Task4();
+    task4.Main();
+
+    Console.WriteLine();
+
+    Task6 task6 = new Task6();
+    task6.Main();
+
+    Console.WriteLine();
+
+    Task7 task7 = new Task7();
+    task7.Main();
+
+    Console.WriteLine();
+
+    Task10 task10 = new Task10();
+    task10.Main();
+
+
 }
