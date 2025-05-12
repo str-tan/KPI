5dd8fbda2bbe8e66525e39c0cfce90858dbbda6c
[КП-43] Стрілецька Тетяна <tanyast06@gmail.com>
2025-05-12 21:28:45 +0300

    Task3.

--- KPIPractice2/KPIPractice2/Generics I/Task3.cs
+++ KPIPractice2/KPIPractice2/Generics I/Task3.cs
@@ -9,6 +9,22 @@
         public void Main()
         {
 
+            var pair1 = new Pair<int, string>(2, "Two");
+            Console.WriteLine(pair1); // [Key: 2, Value: Two]
+
+        }
+    }
+
+    public class Pair<TKey, TValue>
+    {
+        public TKey Key { get; set; }
+        public TValue Value { get; set; }
+
+    
+        public Pair(TKey key, TValue value)
+        {
+            Key = key;
+            Value = value;
         }
     }
 }
