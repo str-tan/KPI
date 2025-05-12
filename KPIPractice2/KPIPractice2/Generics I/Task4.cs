2955b42cce17f2b59e0cff44b8d04c9f3b9e123d
[КП-43] Стрілецька Тетяна <tanyast06@gmail.com>
2025-05-12 21:29:25 +0300

    Task4.

--- KPIPractice2/KPIPractice2/Generics I/Task4.cs
+++ KPIPractice2/KPIPractice2/Generics I/Task4.cs
@@ -13,7 +13,62 @@
     {
         public void Main()
         {
+            ListStorage<int> intStorage = new ListStorage<int>();
+            intStorage.Add(60);
+            intStorage.Add(50);
+            intStorage.Add(20);
+            intStorage.Add(25);
 
+            Console.WriteLine("Int storage:");
+            for (int i = 0; i < intStorage.Count; i++)
+            {
+                Console.WriteLine(intStorage.Get(i));
+            }
+
+            IStorage<string> stringStorage = new ListStorage<string>();
+            stringStorage.Add("Hello");
+            stringStorage.Add("World");
+            stringStorage.Add("Tatiana");
+            stringStorage.Add("KPIPractice2");
+
+            Console.WriteLine("String storage:");
+            for (int i = 0; i < stringStorage.Count; i++)
+            {
+                Console.WriteLine(stringStorage.Get(i));
+            }
+
+        }
+
+        public interface IStorage<T>
+        {
+            void Add(T item);
+            T Get(int index);
+            int Count { get; }
+        }
+
+        public class ListStorage<T> : IStorage<T>
+        {
+            private List<T> smth = new List<T>();
+
+            public void Add(T item)
+            {
+                smth.Add(item);
+            }
+
+            public T Get(int index)
+            {
+                if (index < 0 || index >= smth.Count)
+                    throw new IndexOutOfRangeException("Index is out of range.");
+                return smth[index];
+            }
+
+            public int Count
+            {
+                get
+                {
+                    return smth.Count;
+                }
+            }
         }
     }
 }
