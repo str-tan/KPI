211473845d0e9e969fc95d06da553de275900474
[КП-43] Стрілецька Тетяна <tanyast06@gmail.com>
2025-05-12 21:27:50 +0300

    Task1.

--- KPIPractice2/KPIPractice2/Generics I/Task1.cs
+++ KPIPractice2/KPIPractice2/Generics I/Task1.cs
@@ -9,6 +9,47 @@
     {
         public void Main()
         {
+            Box<int> intBox = new Box<int>(654);
+            intBox.Display();
+
+            Box<string> strBox = new Box<string>("Hello World");
+            strBox.Display();
+
+            Box<Person> personBox = new Box<Person>(new Person("Tatiana", 18));
+            personBox.Display();
+
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+    public class Box<T>
+    {
+        public T Value { get; set; }
+
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Value);
         }
     }
 }
